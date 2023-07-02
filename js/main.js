0d8be531826196e552328cd816eb36f8607ab535
Jalal Jonaid <jalaljonaid@Jalals-MacBook-Air.local>
2023-07-02 07:10:23 -0400

    Added intial JS code to record entries

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const itemForm = document.getElementById('item-form');
+const itemList = document.getElementById('items-list');
+
+const itemName = document.getElementById('name').value;
+const itemPrice = document.getElementById('price').value;
+const itemStock = document.getElementById('stock').value;
+
+const listItem = document.createElement('li');
+listItem.innerHTML = `<strong>${itemName}</strong> - Price: $${itemPrice} - In Stock: ${itemStock}`;
+
+itemList.appendChild;
