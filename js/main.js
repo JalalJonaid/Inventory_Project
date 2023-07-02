8de6d8f840776181894a4c02158df77677200ae6
Jalal Jonaid <jalaljonaid@Jalals-MacBook-Air.local>
2023-07-02 07:56:27 -0400

    Added stock buttons HTML + JS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-const itemForm = document.getElementById('item-form');
-const itemList = document.getElementById('items-list');
+// const itemForm = document.getElementById('item-form');
+// const itemList = document.getElementById('items-list');
 
-itemForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+// itemForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
 
-    const itemName = document.getElementById('name').value;
-    const itemPrice = document.getElementById('price').value;
-    const itemStock = document.getElementById('stock').value;
+//     const itemName = document.getElementById('name').value;
+//     const itemPrice = document.getElementById('price').value;
+//     const itemStock = document.getElementById('stock').value;
 
-const listItem = document.createElement('li');
-listItem.innerHTML = `<strong>${itemName}</strong> - Price: $${itemPrice} - In Stock: ${itemStock}`;
+// const listItem = document.createElement('li');
+// listItem.innerHTML = `<strong>${itemName}</strong> - Price: $${itemPrice} - In Stock: ${itemStock}`;
 
-itemList.appendChild(listItem);
-});
+// itemList.appendChild(listItem);
+// });
+
+function changeStock(item, action) {
+    const stockElement = document.getElementById(`${item}-stock`);
+    let stock = parseInt(stockElement.textContent);
+
+    if (action === 'add') {
+        stock++;
+    }
+    else if (action === 'subtract'){
+        if (stock > 0) {
+            stock--;
+        }
+    }
+
+    stockElement.textContent = stock;
+}
